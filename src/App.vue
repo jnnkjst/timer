84603d5ce89a0aebd47ee8e7cc217d3b24b4bc97
Jannik Jost <jannik-jost@gmx.de>
2022-02-15 16:50:28 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="todoapp">
+    <section>
+      <button @click="start">Start new Countdown</button>
+      <Item
+        v-for="it in items"
+        :key="it.id"
+        :id="it.id"
+        :countdown="it.countdown"
+      />
+    </section>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Item from "./components/Item.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Item },
+  data() {
+    return {
+      items: [{ id: 0, countdown: 20 }],
+      intervalId: "",
+    };
+  },
+  mounted() {
+    this.startInterval();
+  },
+  methods: {
+    start() {
+      this.items.push({ id: Math.random(), countdown: 20 });
+      if (!this.intervalId) {
+        this.startInterval();
+      }
+    },
+    startInterval() {
+      this.intervalId = setInterval(
+        function () {
+          this.items.forEach((element) => {
+            element.countdown -= 1;
+            if (element.countdown === 0) {
+              this.remove(element.id);
+            }
+          });
+        }.bind(this),
+        1000
+      );
+    },
+    remove(id) {
+      const i = this.items.findIndex((el) => el.id === id);
+      this.items.splice(i, 1);
+      if (this.items.length === 1) {
+        clearInterval(this.intervalId);
+        this.intervalId = "";
+      }
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Item</h1>
+    {{ countdown }}
+  </div>
+</template>
+<script>
+export default {
+  props: { id: Number, countdown: Number },
+  data() {
+    return {
+      c: 90,
+    };
+  },
+};
+</script>
